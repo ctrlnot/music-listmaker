d95febe36bcd2eb5e8746ac071d30febb1617c4d
ctrlnot <tonton.perocho@gmail.com>
2018-06-23 18:12:35 +0800

    Added generator object.

--- config.py
+++ config.py
@@ -1,22 +1,14 @@
 # Music Directory
-music_directory = '/home/xs-dev/Music/Music'
+music_directory = ''
 
-generator_folder = 'generated'
+# Folder to store the generated text
+generator_folder = ''
 
-# Filename of text to be generated
-# Note:
-#   Created `generated` folder first.
-#   The txt file is advised to store inside `generated` folder to be ignored by .gitignore
-output_file = 'generated/music-list.txt'
-
-# Temporary text file to store all music nested in folders inside music directory
-nested_music_file = 'generated/inside-folder-music.txt'
-
-# Temporary text file to store all music in the root/music directory
-root_music_file = 'generated/inside-folder-music.txt'
+# Output filename
+output_filename = ''
 
 # Expected audio file types
 # Note:
-#   The list is incomplete because he is lazy af to write all of them and to research more general solution
+#   The list is incomplete because dev is lazy af to write all of them and to research more general solution
 #   Just add if something isn't included 
 audio_file_types = ('.mp3', '.m4a', '.flac', '.wav')
--- generator.py
+++ generator.py
@@ -1,19 +1,20 @@
 import os
+import math
 import errno
 import config
-# from config import audio_file_types
-# from config import generator_folder
 
 class Generator:
   total = 0
   subdirs = []
-  output_file = None
+  output_path = None
 
   def __init__(self, root):
-    self.create_generator_folder()
-    self.find_subdirs(root)
+    self.initialize(root)
 
-    self.list_files()
+  def initialize(self, dir_):
+    self.find_subdirs(dir_)
+    self.create_generator_folder()
+    self.output_path = '{}/{}.txt'.format(config.generator_folder, config.output_filename)
 
   def find_subdirs(self, folder_path):
     dir_ = os.listdir(folder_path)
@@ -26,14 +27,16 @@ class Generator:
         self.find_subdirs(file_path)
 
   def list_files(self):
-    self.create_generator_folder()
-
-    with open(config.output_filename, 'w') as output_file:
-      for dir_ in subdirs:
+    with open(self.output_path, 'w', encoding = "utf-8") as output_file:
+      for dir_ in self.subdirs:
         files = os.listdir(dir_)
+        dir_name = os.path.basename(os.path.normpath(dir_))
+        dir_text = self.addPadding(dir_name) + '\n\n'
+
+        output_file.write(dir_text)
 
         for file_ in files:
-          title = file_ + '\n'
+          title = file_ + '\n\n'
 
           if file_.endswith(config.audio_file_types):
             output_file.write(title)
@@ -48,5 +51,19 @@ class Generator:
       if e.errno != errno.EEXIST:
         raise
 
-  def get_subdirs(self):
-    return sorted(self.subdirs)
+  def addPadding(self, dirName):
+    newDirName = '   ' + dirName + '   '
+    pad = '#'
+    dirLen = len(newDirName)
+
+    maxLen = 75
+    spaceLeft = (maxLen - dirLen) / 2
+    leftPadLen = math.floor(spaceLeft)
+    rightPadLen = int(spaceLeft)
+
+    leftPad = pad.ljust(leftPadLen, pad)
+    rightPad = pad.rjust(leftPadLen, pad)
+
+    newDirName = leftPad + newDirName + rightPad
+
+    return newDirName
